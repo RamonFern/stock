409cfd4f7e7c402fd3886063eab0fb6121066c2c
ramon <ramon@petize.com.br>
2023-11-20 17:23:14 -0300

    Tela login inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app',
+    loadChildren: () => import('./presentation/modules/application/application.module').then((m) => m.ApplicationModule),
+    // canLoad: [AuthGuard],
+    // data: {
+    //     persona: 'Colaborador'
+    // }
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./presentation/modules/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/presentation/modules/login/page/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+    // dashboardEnum: Record<string, any> = Dashboard;
+    formLogin = new FormGroup({
+        login: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+    });
+
+    loading = true;
+
+    constructor(
+        // private loginService: LoginService,
+        // private authService: AuthService,
+        private router: Router,
+        private snackBar: MatSnackBar,
+        // private pacienteService: PacienteService
+    ) {}
+
+    ngOnInit(): void {
+        // if (this.authService.isRefreshTokenValid()) {
+        //     setTimeout(() => {
+        //         const usuarioLogado = JSON.parse(localStorage.getItem('currentUser')!);
+        //         this.router.navigate([this.dashboardEnum[usuarioLogado.dashboardPadrao]]);
+        //     }, 1000);
+        // } else {
+        //     this.loading = false;
+        // }
+    }
+
+    login() {
+        const loginResquest = {
+            login: this.formLogin.controls['login'].value,
+            password: this.formLogin.controls['password'].value,
+        };
+        // if (!this.formLogin.invalid) {
+        //     this.loginService.realizarLogin(loginResquest).subscribe(
+        //         (response) => {
+        //             const dashboardUsuario = response?.body?.user?.dashboardPadrao;
+
+        //             if (!dashboardUsuario) {
+        //                 this.snackBar.open('Erro', 'Seu usuário está com dados inconsistentes.');
+        //                 return;
+        //             }
+        //             this.router.navigate([this.dashboardEnum[dashboardUsuario]]);
+        //         },
+        //         (responseError: HttpErrorResponse) => {
+        //             this.snackBar.open('Erro', 'Login e/ou senha incorretos', { duration: 5000 });
+        //         }
+        //     );
+        // }
+    }
+}
